bcf59dc95a9bb7c4e0bb6a0a7e71e81862d6e039
MedRed <63717981+med-red-09812@users.noreply.github.com>
2026-08-20 19:36:45 +1000

    fix(actions): call FindComponent directly on pOwnerEntity rather than weapon component

--- Scripts/Game/Actions/BGONE_ArmingDistanceSwitchUserAction.c
+++ Scripts/Game/Actions/BGONE_ArmingDistanceSwitchUserAction.c
@@ -1,20 +1,15 @@
 class BGONE_ArmingDistanceSwitchUserAction : ScriptedUserAction
 {
-	protected BaseWeaponComponent m_WeaponComponent;
 	protected BGONE_GuidedMissileLauncherComponent m_LauncherComponent;
 	
 	override void Init(IEntity pOwnerEntity, GenericComponent pManagerComponent)
 	{
-		m_WeaponComponent = BaseWeaponComponent.Cast(pOwnerEntity.FindComponent(BaseWeaponComponent));
-		if(m_WeaponComponent)
-			m_LauncherComponent = BGONE_GuidedMissileLauncherComponent.Cast(m_WeaponComponent.FindComponent(BGONE_GuidedMissileLauncherComponent));
+		if(pOwnerEntity)
+			m_LauncherComponent = BGONE_GuidedMissileLauncherComponent.Cast(pOwnerEntity.FindComponent(BGONE_GuidedMissileLauncherComponent));
 	}
 	
 	override bool CanBeShownScript(IEntity user)
 	{
-		if(!m_LauncherComponent && m_WeaponComponent)
-			m_LauncherComponent = BGONE_GuidedMissileLauncherComponent.Cast(m_WeaponComponent.FindComponent(BGONE_GuidedMissileLauncherComponent));
-			
 		if(!m_LauncherComponent)
 			return false;
 			
@@ -23,9 +18,6 @@ class BGONE_ArmingDistanceSwitchUserAction : ScriptedUserAction
 	
 	override bool GetActionNameScript(out string outName)
 	{
-		if(!m_LauncherComponent && m_WeaponComponent)
-			m_LauncherComponent = BGONE_GuidedMissileLauncherComponent.Cast(m_WeaponComponent.FindComponent(BGONE_GuidedMissileLauncherComponent));
-			
 		if(!m_LauncherComponent)
 			return false;
 			
@@ -35,8 +27,8 @@ class BGONE_ArmingDistanceSwitchUserAction : ScriptedUserAction
 	
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
-		if(!m_LauncherComponent && m_WeaponComponent)
-			m_LauncherComponent = BGONE_GuidedMissileLauncherComponent.Cast(m_WeaponComponent.FindComponent(BGONE_GuidedMissileLauncherComponent));
+		if(!m_LauncherComponent && pOwnerEntity)
+			m_LauncherComponent = BGONE_GuidedMissileLauncherComponent.Cast(pOwnerEntity.FindComponent(BGONE_GuidedMissileLauncherComponent));
 			
 		if(!m_LauncherComponent)
 			return;
